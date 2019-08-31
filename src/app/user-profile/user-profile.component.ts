f1488cc3278e126208c21670cc4424b67695491f
juru-juliette <kangabejuliette@gmail.com>
2019-08-31 18:23:07 +0200

    adding http request

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,19 +1,49 @@
+// import { Component, OnInit } from '@angular/core';
+// import {User}from '../user';
+// import {HttpClient} from '@angular/common/http';
+// import {HttpRequestService}from './repos-request';
+// import { Repository } from '../repository';
+
+
+// @Component({
+//   selector: 'app-user-profile',
+//   templateUrl: './user-profile.component.html',
+//   providers:[HttpRequestService],
+//   styleUrls: ['./user-profile.component.css']
+ 
+// })
+// export class UserProfileComponent implements OnInit {
+//   title = 'GitSearch';
+//   User:User;
+//   repos:Repository;
+//   addNew(user){
+//     this.reposRequest.reposRequest(user)
+//     this.repos=this.reposRequest.repos
+//     // this.User.push(User)
+//     this.Users= new User(user.name)
+//     // console.log(user)
+ 
+//   constructor(private repoRequest:HttpRequestService) { 
+//     this.Users= new User("");
+//     this.repos=new Repository(0,0,0,new Date(),"","");
+//   }
+
+//   ngOnInit() {
+//   }
+
+// }
 import { Component, OnInit } from '@angular/core';
 import {User}from '../user';
 import {HttpClient} from '@angular/common/http';
-import {HttpRequestService}from '../repos-request/http-request.service'
+import {HttpRequestService}from './repos-request'
 import { Repository } from '../repository';
-
-
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
+  selector: 'app-github',
+  templateUrl: './github.component.html',
   providers:[HttpRequestService],
-  styleUrls: ['./user-profile.component.css'],
- 
+  styleUrls: ['./github.component.css']
 })
-export class UserProfileComponent implements OnInit {
-  title = 'GitSearch';
+export class GithubComponent implements OnInit {
   Users:User;
   repos:Repository;
   addNew(user){
@@ -22,13 +52,15 @@ export class UserProfileComponent implements OnInit {
     // this.User.push(User)
     this.Users= new User(user.name)
     // console.log(user)
- 
-  constructor(private repoRequest:HttpRequestService) { 
+}
+  constructor(private reposRequest:HttpRequestService) { 
     this.Users= new User("");
     this.repos=new Repository(0,0,0,new Date(),"","");
   }
 
   ngOnInit() {
+    
   }
 
 }
+
